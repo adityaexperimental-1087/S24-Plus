659145f0773c6ca9da68e3ebcab196921fb23198
adi1087 <adityaexperimental@gmail.com>
2025-08-11 18:07:42 +0530

    Update customize.sh

--- unica/patches/camera/customize.sh
+++ unica/patches/camera/customize.sh
@@ -1,10 +1,11 @@
-if $SOURCE_HAS_MASS_CAMERA_APP; then
-    if ! $TARGET_HAS_MASS_CAMERA_APP; then
-        ADD_TO_WORK_DIR "e2sxxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
-        ADD_TO_WORK_DIR "e2sxxx" "system" "system/priv-app/SamsungCamera/oat"
+if ! $SOURCE_HAS_MASS_CAMERA_APP; then
+    if $TARGET_HAS_MASS_CAMERA_APP; then
+        DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/oat"
+        DELETE_FROM_WORK_DIR "system" "system/priv-app/SamsungCamera/SamsungCamera.apk.prof"
+        ADD_TO_WORK_DIR "r12sxxx" "system" "system/priv-app/SamsungCamera/SamsungCamera.apk" 0 0 644 "u:object_r:system_file:s0"
     else
-        LOG "- TARGET_HAS_MASS_CAMERA_APP is set. Ignoring."
+        LOG "- TARGET_HAS_MASS_CAMERA_APP is not set. Ignoring"
     fi
 else
-    LOG "- SOURCE_HAS_MASS_CAMERA_APP is not set. Ignoring."
+    LOG "- SOURCE_HAS_MASS_CAMERA_APP is set. Ignoring"
 fi
