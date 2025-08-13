9ddf25416440e73d437121ebbe26fe62e3531453
adi1087 <adityaexperimental@gmail.com>
2025-08-14 00:02:09 +0530

    Update version.sh

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -15,9 +15,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-VERSION_MAJOR=26 e2s
+VERSION_MAJOR=26
 VERSION_MINOR=1
-VERSION_PATCH=0
+VERSION_PATCH=1
 
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 # Append current commit hash to version name
