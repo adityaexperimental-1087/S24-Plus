cc16f2721a48f66999a0e41200bf48b9fc106f53
adi1087 <adityaexperimental@gmail.com>
2025-08-19 06:19:58 +0530

    Update version.sh

--- unica/configs/version.sh
+++ unica/configs/version.sh
@@ -17,10 +17,10 @@
 
 VERSION_MAJOR=26
 VERSION_MINOR=1
-VERSION_PATCH=1
+VERSION_PATCH=0
 
 ROM_VERSION="${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
 # Append current commit hash to version name
 ROM_VERSION+="-$(git rev-parse --short HEAD)"
 
-ROM_CODENAME="Fishe"
+ROM_CODENAME="Peace"
